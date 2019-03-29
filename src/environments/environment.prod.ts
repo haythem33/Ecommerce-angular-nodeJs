d84e551d047d3ddad573453fd415c59de94981bd
haythem33 <esperance191@hotmail.fr>
2019-03-29 22:33:57 +0100

    serveur

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url : 'http://192.168.137.65:3000',
-  api_url_socket : 'http://192.168.137.65:3000'
+  api_url : 'http://192.168.81.128:3000',
+  api_url_socket : 'http://192.168.81.128:3000'
 };
