b7dda93b9f4d481ce9720019f423d14a4a252c1d
haythem33 <esperance191@hotmail.fr>
2019-03-29 23:20:53 +0100

    serveur

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url : 'http://192.168.81.128:3000',
-  api_url_socket : 'http://192.168.81.128:3000'
+  api_url : 'http://192.168.1.101:3000',
+  api_url_socket : 'http://192.168.1.101:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  api_url : 'http://192.168.81.128:3000',
-  api_url_socket : 'http://192.168.81.128:3000'
+  api_url : 'http://192.168.1.101:3000',
+  api_url_socket : 'http://192.168.1.101:3000'
 };
 
 /*
