4dfccdc01eca45bfc98ad3ca9c7d0583c7c265ab
haythem ben jbara <42939839+haythem33@users.noreply.github.com>
2019-08-23 04:50:53 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  api_url : 'http://localhost:3000',
-  api_url_socket : 'http://localhost:3000'
+  api_url : 'http://192.168.230.129:3000',
+  api_url_socket : 'http://192.168.230.129:3000'
 };
 
 /*
